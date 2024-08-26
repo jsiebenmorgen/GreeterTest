6433d0300ebddbe365528b0cc01b9d4b932bcb65
jsiebenmorgen <157930232+jsiebenmorgen@users.noreply.github.com>
2024-08-26 15:41:23 -0500

    Initial commit

---
+++ src/Greeter.java
@@ -0,0 +1,13 @@
+// Test Assignment
+// Description: program to print "hello world!" Message to console.
+
+public class Greeter {
+    public static void main(String[] args) {
+
+        //declaration and initialization
+        String greeting1 = "Hello World!";
+    //Print message to console
+    System.out.print(greeting1);
+
+    }
+}
---
+++ src/notes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class notes {
+
+
+    Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+}
